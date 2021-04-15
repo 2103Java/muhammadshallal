146f957e2f2d623f2d45f23aa9df871bd554faf5
hassanshallal <hshallal@icloud.com>
2021-04-15 13:45:38 -0600

    00_initial_sts_maven_structure

---
+++ ERS/src/main/java/com/revature/ajax/ClientMessage.java
@@ -0,0 +1,5 @@
+package com.revature.ajax;
+
+public class ClientMessage {
+
+}
---
+++ ERS/src/main/java/com/revature/request/DispatcherServlet.java
@@ -0,0 +1,10 @@
+package com.revature.request;
+
+public class DispatcherServlet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
